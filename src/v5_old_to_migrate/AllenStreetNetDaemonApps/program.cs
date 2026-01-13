2bd68e80a2d84e4461cd6df42179d76f0071a1d8
DSikes <pockybum522@gmail.com>
2026-01-13 07:24:26 -0500

    Moved old v5 project to folder, made new default v6 project

--- src/v5_old_to_migrate/AllenStreetNetDaemonApps/program.cs
+++ src/v5_old_to_migrate/AllenStreetNetDaemonApps/program.cs
@@ -1,10 +1,15 @@
 using System.Reflection;
+using AllenStreetNetDaemonApps;
+using AllenStreetNetDaemonApps.EntityWrappers;
+using AllenStreetNetDaemonApps.EntityWrappers.Lights;
+using AllenStreetNetDaemonApps.EntityWrappers.Lights.Interfaces;
+using HomeAssistantGenerated;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NetDaemon.Extensions.Logging;
 using NetDaemon.Extensions.Scheduler;
 using NetDaemon.Extensions.Tts;
 using NetDaemon.Runtime;
-using HomeAssistantGenerated;
 
 #pragma warning disable CA1812
 
@@ -17,6 +22,10 @@ try
         .UseNetDaemonTextToSpeech()
         .ConfigureServices((_, services) =>
             services
+                .AddSingleton<IKitchenLightsWrapper, KitchenLightsWrapper>()
+                .AddSingleton<IFrontRoomLightsWrapper, FrontRoomLightsWrapper>()
+                .AddSingleton<IGuestBathLightsWrapper, GuestBathLightsWrapper>()
+                .AddSingleton<IMasterBathLightsWrapper, MasterBathLightsWrapper>()
                 .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
                 .AddNetDaemonStateManager()
                 .AddNetDaemonScheduler()
