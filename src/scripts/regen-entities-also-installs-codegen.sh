c8dd3e9c17ffa9bde525ced7922555d8fa1fe848
DSikes <pockybum522@gmail.com>
2026-09-15 11:40:32 -0400

    Fixed nd-codegen call to go into correct directory

--- src/scripts/regen-entities-also-installs-codegen.sh
+++ src/scripts/regen-entities-also-installs-codegen.sh
@@ -3,11 +3,8 @@
 
 /home/david/.dotnet/dotnet tool update -g NetDaemon.HassModel.CodeGen
 
-cd ../AllenStreetNetDaemonApps
-
+cd /media/secondary/repos/netdaemon-home-assistant-apps/src/AllenStreetNetDaemonApps
 
 # Add check to see if it needs to be restored, here
 
-
-/home/david/.dotnet/dotnet tool run nd-codegen
-
+nd-codegen
